37d8f175928325b083370e0071bd596a19bc5bbf
최승준 <csjune99@naver.com>
2024-10-21 19:03:35 +0900

    [FE] 💄 design: viewModel의 UiState 관리 로직 변경

--- Android/feature/home/src/main/java/com/ssafy/home/home/HomeViewModel.kt
+++ Android/feature/home/src/main/java/com/ssafy/home/home/HomeViewModel.kt
@@ -3,12 +3,12 @@ package com.ssafy.home.home
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
-    var uiState = MutableStateFlow(HomeUiState())
-        private set
-
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState = _uiState.asStateFlow()
 
 }
