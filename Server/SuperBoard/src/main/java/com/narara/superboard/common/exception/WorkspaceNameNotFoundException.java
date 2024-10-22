3e20379f51030a0496684a0cfcc7485c3f7cbe51
조시현 <si4018@naver.com>
2024-10-22 14:17:42 +0900

    refactor: 사용하지 않는 메서드 삭제

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/exception/WorkspaceNameNotFoundException.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/exception/WorkspaceNameNotFoundException.java
@@ -1,9 +1,6 @@
 package com.narara.superboard.common.exception;
 
 public class WorkspaceNameNotFoundException extends NotFoundException {
-    public WorkspaceNameNotFoundException(String message) {
-        super(message);
-    }
     public WorkspaceNameNotFoundException() {
         super("워크스페이스", "이름");
     }
