cd26b9d43153bcf172ebb39d6e3c07e952c12f62
최승준 <csjune99@naver.com>
2024-10-22 12:12:51 +0900

    [FE] 💄 design: DrawerSheet 홈 화면에 추가

--- Android/feature/home/src/main/java/com/ssafy/home/home/Home.kt
+++ Android/feature/home/src/main/java/com/ssafy/home/home/Home.kt
@@ -2,29 +2,23 @@ package com.ssafy.home.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.ssafy.designsystem.values.CornerLarge
-import com.ssafy.designsystem.values.ElevationMedium
-import com.ssafy.designsystem.values.Primary
-import com.ssafy.designsystem.values.TextMedium
+import coil3.compose.AsyncImage
 import com.ssafy.designsystem.values.White
+import com.ssafy.home.drawer.DrawerSheet
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -52,58 +46,60 @@ private fun HomeScreen(
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
     val spanCount = if (isPortrait) 2 else 4
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        containerColor = White,
-        topBar = {
-            MainTopBar(
-                onDrawerClick = { /*TODO*/ },
-                onSearchClick = { /*TODO*/ },
-                onAlarmClick = { /*TODO*/ },
-                onMenuClick = { /*TODO*/ }
+    val url =
+        "https://an2-img.amz.wtchn.net/image/v2/h6S3XfqeRo7KBUmE9ArtBA.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1USTRNSGczTWpCeE9EQWlYU3dpY0NJNklpOTJNaTl6ZEc5eVpTOXBiV0ZuWlM4eE5qRTFPRGN5T0RNd05UazJOVFF4TWpRNUluMC5OOTZYYXplajFPaXdHaWFmLWlmTjZDU1AzczFRXzRQcW4zM0diQmR4bC1z"
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerSheet(
+                icon = { AsyncImage(model = url, contentDescription = null, contentScale= ContentScale.Crop) },
+                nickname = "손오공",
+                email = "monkey@naver.com",
+                workspaceList = List(4) { "손오공's workspace" },
+                onAddWorkSpaceClick = { /*TODO*/ },
+                onMyBoardClick = { /*TODO*/ },
+                onMyCardClick = { /*TODO*/ },
+                onSettingClick = { /*TODO*/ },
+                onLogoutClick = { /*TODO*/ },
+                onWorkSpaceClick = { /*TODO*/ }
             )
-        },
-        floatingActionButton = {
+        }
+    ) {
+        Scaffold(
+            containerColor = White,
+            topBar = {
+                MainTopBar(
+                    onDrawerClick = { scope.launch { drawerState.open() } },
+                    onSearchClick = { /*TODO*/ },
+                    onAlarmClick = { /*TODO*/ },
+                    onMenuClick = { /*TODO*/ }
+                )
+            },
+            floatingActionButton = {
+                if (workSpace != null) {
+                    AddNewBoardFloatingButton(
+                        moveToCreateNewBoardScreen = moveToCreateNewBoardScreen
+                    )
+                }
+            }
+        ) { innerPadding ->
             if (workSpace != null) {
-                ExtendedFloatingActionButton(
-                    containerColor = Primary,
-                    contentColor = White,
-                    elevation = FloatingActionButtonDefaults.elevation(
-                        defaultElevation = ElevationMedium
-                    ),
-                    shape = RoundedCornerShape(CornerLarge),
-                    onClick = moveToCreateNewBoardScreen,
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = "Create new board"
-                        )
-                    },
-
-                    text = {
-                        Text(
-                            text = "Create Board",
-                            fontSize = TextMedium,
-                            fontWeight = FontWeight.Bold
-                        )
-                    },
+                HomeBodyScreen(
+                    modifier = Modifier.padding(innerPadding),
+                    // TODO : Board에 대한 정보를 전달합니다.
+                    boards = List(4) { Any() },
+                    spanCount = spanCount,
+                    moveToBoardScreen = moveToBoardScreen
+                )
+            } else {
+                HomeEmptyScreen(
+                    modifier = Modifier.padding(innerPadding),
+                    moveToCreateNewWorkSpaceScreen = moveToCreateNewWorkSpaceScreen
                 )
             }
-        }
-    ) { innerPadding ->
-        if (workSpace != null) {
-            HomeBodyScreen(
-                modifier = Modifier.padding(innerPadding),
-                // TODO : Board에 대한 정보를 전달합니다.
-                boards = List(4) { Any() },
-                spanCount = spanCount,
-                moveToBoardScreen = moveToBoardScreen
-            )
-        } else {
-            HomeEmptyScreen(
-                modifier = Modifier.padding(innerPadding),
-                moveToCreateNewWorkSpaceScreen = moveToCreateNewWorkSpaceScreen
-            )
         }
     }
 }
