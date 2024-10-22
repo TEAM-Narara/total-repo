ceedb651064c969841c9c360a374ff254d8c075a
조시현 <si4018@naver.com>
2024-10-22 14:18:45 +0900

    feat: 워크스페이스 예외 처리 로직 추가

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/worksapce/service/WorkSpaceValidator.java
@@ -0,0 +1,14 @@
+package com.narara.superboard.worksapce.service;
+
+import com.narara.superboard.common.exception.WorkspaceNameNotFoundException;
+import com.narara.superboard.worksapce.interfaces.dto.CreateWorkspaceDto;
+
+public class WorkSpaceValidator {
+
+    public void validateCreateDto(CreateWorkspaceDto createWorkspaceDto) {
+        // 이름이 null 또는 공백이거나 빈 문자열일 때 예외 처리
+        if (createWorkspaceDto.name() == null || createWorkspaceDto.name().trim().isEmpty()) {
+            throw new WorkspaceNameNotFoundException();
+        }
+    }
+}
