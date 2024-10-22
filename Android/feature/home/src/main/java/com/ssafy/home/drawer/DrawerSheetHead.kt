44dd33fc3cbb700a60ad13f22b357db21bb3a053
최승준 <csjune99@naver.com>
2024-10-22 13:16:39 +0900

    [FE] 💄 design: DrawerSheet 로그아웃 아이콘 변경

--- Android/feature/home/src/main/java/com/ssafy/home/drawer/DrawerSheetHead.kt
+++ Android/feature/home/src/main/java/com/ssafy/home/drawer/DrawerSheetHead.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -48,7 +48,7 @@ fun DrawerSheetHead(
                 .align(Alignment.TopEnd)
         ) {
             Icon(
-                imageVector = Icons.AutoMirrored.Default.ArrowForward,
+                imageVector = Icons.AutoMirrored.Default.Logout,
                 tint = White,
                 contentDescription = "로그아웃"
             )
