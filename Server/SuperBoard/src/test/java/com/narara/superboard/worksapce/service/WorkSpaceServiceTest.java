17cc230b4a2f0c4badea6d86b7643c02297c6674
조시현 <si4018@naver.com>
2024-10-22 15:44:58 +0900

    test: 이름이 없는 상황에대한 에러 발생 테스트 이전

--- Server/SuperBoard/src/test/java/com/narara/superboard/worksapce/service/WorkSpaceServiceTest.java
+++ Server/SuperBoard/src/test/java/com/narara/superboard/worksapce/service/WorkSpaceServiceTest.java
@@ -4,11 +4,15 @@ package com.narara.superboard.worksapce.service;
 import com.narara.superboard.common.exception.WorkspaceNameNotFoundException;
 import com.narara.superboard.worksapce.infrastructure.WorkSpaceRepository;
 import com.narara.superboard.worksapce.interfaces.dto.WorkspaceCreateDto;
+import com.narara.superboard.worksapce.interfaces.dto.WorkspaceUpdateRequestDto;
+import com.narara.superboard.worksapce.service.validator.WorkSpaceValidator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,23 +23,14 @@ class WorkSpaceServiceTest {
 
     // 실제 테스트 대상 객체를 생성하고 초기화할 때 사용
     @InjectMocks
-    private WorkSpaceService workSpaceService; // 테스트 대상 클래스
+    private WorkSpaceServiceImpl workSpaceService; // 테스트 대상 클래스
 
-    @Mock
-    private WorkSpaceRepository workspaceRepository;
-
-
-    @DisplayName("워크스페이스 생성 시, 이름이 없으면 에러가 발생한다.")
-    @ParameterizedTest
-    @CsvSource({
-            ", '워크스페이스 설명'",  // 이름이 없는 경우
-            "'', '워크스페이스 설명'"  // 이름이 빈 문자열인 경우
-    })
-    void testWorkSpaceEntityCreation(String name, String description) {
-        WorkspaceCreateDto workspaceCreateDto = new WorkspaceCreateDto(name, description);
-
-        assertThrows(WorkspaceNameNotFoundException.class, () -> workSpaceService.createWorkSpace(workspaceCreateDto));
+    @BeforeEach
+    void setUp() {
+        // Mockito가 Mock 객체를 초기화
+        MockitoAnnotations.openMocks(this);
     }
+
 
     @DisplayName("워크스페이스가 정상적으로 생성된다.")
     @ParameterizedTest
@@ -53,27 +48,5 @@ class WorkSpaceServiceTest {
         // then
         // 메서드가 한 번 호출되었는지 확인
         verify(workSpaceService, times(1)).createWorkSpace(workspaceCreateDto);
-    }
-
-
-    @DisplayName("워크스페이스 수정 시, 이름이 없으면 에러가 발생한다.")
-    @ParameterizedTest
-    @CsvSource({
-            ", '워크스페이스 설명'",  // 이름이 없는 경우
-            "'', '워크스페이스 설명'"  // 이름이 빈 문자열인 경우
-    })
-    void testUpdateWorkspaceNameError(String name, String description) {
-        // given
-        Long workspaceId = 1L;
-        WorkspaceUpdateDto dto = new WorkspaceUpdateDto(name, description);
-
-        // when & then
-        // JUnit의 메서드로, 실제로 예외가 발생하는지 검증하는 코드
-        assertThrows(WorkspaceNameNotFoundException.class, () -> {
-            workSpaceService.updateWorkspace(workspaceId, dto);
-        });
-
-        // 서비스 호출 검증
-        verify(workSpaceService, times(1)).updateWorkspace(workspaceId, dto);
     }
 }
---
+++ Server/SuperBoard/src/test/java/com/narara/superboard/worksapce/service/validator/WorkSpaceValidatorTest.java
@@ -0,0 +1,49 @@
+package com.narara.superboard.worksapce.service.validator;
+
+import com.narara.superboard.common.exception.WorkspaceNameNotFoundException;
+import com.narara.superboard.worksapce.interfaces.dto.WorkspaceCreateDto;
+import com.narara.superboard.worksapce.interfaces.dto.WorkspaceUpdateRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("워크스페이스 검증 테스트")
+class WorkSpaceValidatorTest {
+
+    @InjectMocks
+    private WorkSpaceValidator workSpaceValidator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);  // Mockito 초기화
+    }
+
+    @DisplayName("생성 DTO에 이름이 없으면 에러가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            ", '워크스페이스 설명'",  // 이름이 없는 경우
+            "'', '워크스페이스 설명'"  // 이름이 빈 문자열인 경우
+    })
+    void testWorkSpaceEntityCreation(String name, String description) {
+        WorkspaceCreateDto workspaceCreateDto = new WorkspaceCreateDto(name, description);
+
+        assertThrows(WorkspaceNameNotFoundException.class, () -> workSpaceValidator.validateNameIsPresent(workspaceCreateDto));
+    }
+
+    @DisplayName("수정 DTO에 이름이 없으면 에러가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            ", '워크스페이스 설명'",  // 이름이 없는 경우
+            "'', '워크스페이스 설명'"  // 이름이 빈 문자열인 경우
+    })
+    void testWorkSpaceEntityUpdate(String name, String description) {
+        WorkspaceUpdateRequestDto workspaceUpdateRequestDto = new WorkspaceUpdateRequestDto(name, description);
+
+        assertThrows(WorkspaceNameNotFoundException.class, () -> workSpaceValidator.validateNameIsPresent(workspaceUpdateRequestDto));
+    }
+}
