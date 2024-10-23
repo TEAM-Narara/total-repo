1e61ac505720501ac4c071533e310f5951eee4cb
조시현 <si4018@naver.com>
2024-10-23 13:24:18 +0900

    feat: 관계 테이블에서 각 관계에 대해서 리스트를 찾아주는 기능 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/infrastructure/WorkSpaceMemberRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/infrastructure/WorkSpaceMemberRepository.java
@@ -1,7 +1,11 @@
 package com.narara.superboard.workspacemember.infrastructure;
 
-import com.narara.superboard.workspacemember.entity.WorkspaceMember;
+import com.narara.superboard.workspacemember.entity.WorkSpaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkSpaceMemberRepository extends JpaRepository<WorkspaceMember, Long> {
+import java.util.List;
+
+public interface WorkSpaceMemberRepository extends JpaRepository<WorkSpaceMember, Long> {
+    List<WorkSpaceMember> findAllByWorkSpaceId(Long workSpaceId);
+    List<WorkSpaceMember> findAllByMemberId(Long memberId);
 }
