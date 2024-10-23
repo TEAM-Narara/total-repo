f915a348c771ba813b557fa5921937b6a6c0b66d
조시현 <si4018@naver.com>
2024-10-23 10:05:22 +0900

    feat: 회원 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/member/entity/Member.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/member/entity/Member.java
@@ -1,4 +1,28 @@
 package com.narara.superboard.member.entity;
 
+import com.narara.superboard.workspacemember.entity.WorkspaceMember;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "profile_img_url")
+    private String profileImgUrl;
+
+    @OneToMany(mappedBy = "member")
+    private List<WorkspaceMember> workspaceMemberList;
 }
