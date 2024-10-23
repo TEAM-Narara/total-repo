2d425a0ea8d7273d4b435355410662d4afa29e3c
조시현 <si4018@naver.com>
2024-10-23 17:25:43 +0900

    feat: 커버 타입이 잘못된 형식일시 발생시키는 예외 추가

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/exception/InvalidCoverTypeFormatException.java
@@ -0,0 +1,8 @@
+package com.narara.superboard.common.exception;
+
+public class InvalidCoverTypeFormatException extends InvalidFormatException {
+
+    public InvalidCoverTypeFormatException() {
+        super("커버의 타입");
+    }
+}
