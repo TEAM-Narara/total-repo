882b7dd198142faf9863e65430eadf351e5e4587
조시현 <si4018@naver.com>
2024-10-23 13:21:48 +0900

    refactor: JoinColumn 어노테이션으로 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/entity/WorkSpaceMember.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/entity/WorkSpaceMember.java
@@ -1,5 +1,7 @@
 package com.narara.superboard.workspacemember.entity;
 
+import com.narara.superboard.member.entity.Member;
+import com.narara.superboard.workspace.entity.WorkSpace;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,17 +14,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Table(name = "workspace_member")
-public class WorkspaceMember {
+public class WorkSpaceMember {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // 기본 키
 
-    @Column(name = "member_id", nullable = false)
-    private Long memberId;  // 멤버 ID
+    @JoinColumn(name = "member", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;  // 멤버 ID
 
-    @Column(name = "workspace_id", nullable = false)
-    private Long workspaceId;  // 워크스페이스 ID
+    @JoinColumn(name = "workspace", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WorkSpace workSpace;  // 워크스페이스 ID
 
     @Column(name = "authority", nullable = false, length = 50)
     private String authority;  // 권한 (ADMIN, MEMBER)
