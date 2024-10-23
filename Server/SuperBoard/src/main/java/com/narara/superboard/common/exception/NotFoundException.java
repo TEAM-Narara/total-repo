73731d4471972310cae97dce3a442a0f8a5142ce
조시현 <si4018@naver.com>
2024-10-23 16:04:36 +0900

    refactor: 에러 메시지의 가독성 증가

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/exception/NotFoundException.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package com.narara.superboard.common.exception;
 
 public class NotFoundException extends IllegalArgumentException {
     public NotFoundException(String Object, String nonExistentValue) {
-        super(String.format("%s의 %s(이)가 존재하지 않습니다. %s을 작성해주세요.", Object, nonExistentValue, nonExistentValue));
+        super(String.format("%s의 %s(이)가 존재하지 않습니다. %s(을)를 작성해주세요.", Object, nonExistentValue, nonExistentValue));
     }
 }
