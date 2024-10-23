9e851b59d34c7d7b8d79cfda1ff0f2ba78879135
조시현 <si4018@naver.com>
2024-10-23 13:21:08 +0900

    refactor: 현재 mysql에 적용되게 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/board/entity/Board.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/entity/Board.java
@@ -1,9 +1,8 @@
 package com.narara.superboard.board.entity;
 
+import com.narara.superboard.workspace.entity.WorkSpace;
 import jakarta.persistence.*;
 import lombok.Getter;
-
-import java.util.Map;
 
 @Entity
 @Getter
@@ -13,14 +12,18 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // 기본키
 
-    @Column(name = "workspace_id", nullable = false)
-    private Long workspaceId;  // 워크스페이스 키
+    @JoinColumn(name = "workspace_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WorkSpace workSpace;  // 워크스페이스 키
 
     @Column(name = "name", nullable = false, length = 255)
     private String name;  // 이름
 
-    @Column(name = "background", columnDefinition = "jsonb")
-    private Map<String, Object> background;  // 커버 (JSONB)
+    // postgresql 에서 사용 가능.
+//    @Type(JsonBinaryType.class)  // Hibernate Types 라이브러리를 통해 jsonb 타입으로 매핑
+//    @Type(type = "jsonb")  // 정의한 jsonb 타입을 사용
+//    @Column(name = "background", columnDefinition = "jsonb")
+//    private Map<String, Object> background;  // 커버 (JSON)
 
     @Column(name = "last_list_order", nullable = false)
     private Long lastListOrder;  // 보드 내 마지막 리스트 순서
@@ -28,14 +31,17 @@ public class Board {
     @Column(name = "visibility", nullable = false, length = 50)
     private String visibility;  // 가시성 (WORKSPACE, PRIVATE)
 
+    @Version
     @Column(name = "version", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long version;  // 버전
 
     public String getBackGroundType() {
-        return background.get("type").toString();
+        return "postgresql";
+//        return background != null ? background.get("type").toString() : null;
     }
+
     public String getBackGroundValue() {
-        return background.get("value").toString();
+        return "postgresql";
+//        return background != null ? background.get("value").toString() : null;
     }
 }
