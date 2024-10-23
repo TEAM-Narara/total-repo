1cc528d82c74642fc7d29ee999fa705e3b60ef2a
조시현 <si4018@naver.com>
2024-10-23 15:37:29 +0900

    refactor: 보드 생성 DTO에 가시성 추가

--- Server/SuperBoard/src/test/java/com/narara/superboard/board/service/validator/BoardValidatorTest.java
+++ Server/SuperBoard/src/test/java/com/narara/superboard/board/service/validator/BoardValidatorTest.java
@@ -1,6 +1,9 @@
 package com.narara.superboard.board.service.validator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.narara.superboard.board.interfaces.dto.BoardCreateRequestDto;
 import com.narara.superboard.common.exception.WorkspaceNameNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,15 +31,15 @@ class BoardValidatorTest {
     @DisplayName("생성 DTO에 이름이 없으면 에러가 발생한다.")
     @ParameterizedTest
     @CsvSource({
-            ", '{\"type\": \"color\", \"value\": \"#ffffff\"}'",  // 이름이 없는 경우
-            "'', '{\"type\": \"imageurl\", \"value\": \"https://example.com/image.jpg\"}'"  // 이름이 빈 문자열인 경우
+            ", 'PRIVATE', ''{\"type\": \"color\", \"value\": \"#ffffff\"}'",  // 이름이 없는 경우
+            "'', 'WORKSPACE', '{\"type\": \"imageurl\", \"value\": \"https://example.com/image.jpg\"}'"  // 이름이 빈 문자열인 경우
     })
-    void testBoardEntityCreation(String name, String backgroundJson) {
+    void testBoardEntityCreation(String name, String visibility, String backgroundJson) throws JsonProcessingException {
         // JSON을 Map으로 변환
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> background = objectMapper.readValue(backgroundJson, new TypeReference<Map<String, Object>>() {});
 
-        BoardCreateRequestDto boardCreateDto = new BoardCreateRequestDto(name, background);
+        BoardCreateRequestDto boardCreateDto = new BoardCreateRequestDto(name, visibility, background);
 
         assertThrows(WorkspaceNameNotFoundException.class, () -> boardValidator.validateNameIsPresent(boardCreateDto));
     }
