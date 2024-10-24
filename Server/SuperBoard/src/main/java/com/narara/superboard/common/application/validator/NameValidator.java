fd268231305cf561566bf775f1203af25c8eb06b
조시현 <si4018@naver.com>
2024-10-24 15:27:07 +0900

    feat: 빈 객체로 관리 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/application/validator/NameValidator.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/application/validator/NameValidator.java
@@ -2,7 +2,9 @@ package com.narara.superboard.common.application.validator;
 
 import com.narara.superboard.common.exception.NotFoundNameException;
 import com.narara.superboard.common.interfaces.dto.NameHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NameValidator {
 
     public void validateNameIsEmpty(NameHolder nameHolder) {
