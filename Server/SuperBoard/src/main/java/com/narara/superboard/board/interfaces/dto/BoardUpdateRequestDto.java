1d052f0e140a645f48a61a29477877edabd3e9a9
조시현 <si4018@naver.com>
2024-10-24 10:12:37 +0900

    refactor: 보드의 멤버 필드 삭제

--- Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardUpdateRequestDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardUpdateRequestDto.java
@@ -7,6 +7,5 @@ public record BoardUpdateRequestDto(
         String name, // 보드 이름
         Map<String, Object> background, // 배경 (JSON: type과 value)
         String visibility // 가시성 (Workspace, Private)
-//        List<Long> memberIds // 보드 멤버 리스트 (멤버 ID 목록)
 ) {
 }
--- Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardUpdateResponseDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardUpdateResponseDto.java
@@ -8,6 +8,5 @@ public record BoardUpdateResponseDto(
         String name, // 보드 이름
         Map<String, Object> background, // 배경 (JSON)
         String visibility // 가시성
-//        List<Long> memberIds // 수정된 보드 멤버 리스트 (멤버 ID 목록)
 ) {
 }
