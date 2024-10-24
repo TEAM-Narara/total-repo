cb0659f42840adb93228c9b6dc0373714a8bc899
박준식 <akfldkskgorn@gmail.com>
2024-10-24 16:55:39 +0900

    [FE] 💄 design: LazyList smoothScroll 구현

--- Android/feature/board/src/main/java/com/ssafy/board/board/utils.kt
+++ Android/feature/board/src/main/java/com/ssafy/board/board/utils.kt
@@ -1,5 +1,9 @@
 package com.ssafy.board.board
 
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.lazy.LazyListState
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -7,21 +11,43 @@ import kotlinx.coroutines.launch
 suspend fun handleLazyListScroll(
     lazyListState: LazyListState,
     dropIndex: Int,
-    indexOffset: Int = 0,
 ): Unit = coroutineScope {
-    val targetIndex = dropIndex + indexOffset
-
     val viewportSize = lazyListState.layoutInfo.viewportEndOffset - lazyListState.layoutInfo.viewportStartOffset
 
-    val itemInfo = lazyListState.layoutInfo.visibleItemsInfo.firstOrNull { it.index == targetIndex }
+    val itemInfo = lazyListState.layoutInfo.visibleItemsInfo.firstOrNull()
     val itemSize = itemInfo?.size ?: 0
 
-    val centerOffset = (itemSize - viewportSize) / 2
+    val centerOffset = (viewportSize - itemSize) / 2
 
     launch {
-        lazyListState.animateScrollToItem(
-            index = targetIndex,
+        lazyListState.smoothScrollToItem(
+            index = dropIndex,
             scrollOffset = centerOffset,
+        )
+    }
+}
+
+
+suspend fun LazyListState.smoothScrollToItem(
+    index: Int,
+    scrollOffset: Int = 0,
+    duration: Int = 500,
+    easing: Easing = FastOutSlowInEasing
+) {
+    val itemInfo = layoutInfo.visibleItemsInfo.firstOrNull { it.index == index }
+
+    if (itemInfo != null) {
+        animateScrollBy(
+            value = (itemInfo.offset - scrollOffset).toFloat(),
+            animationSpec = tween(
+                durationMillis = duration,
+                easing = easing
+            )
+        )
+    } else {
+        animateScrollToItem(
+            index = index,
+            scrollOffset = scrollOffset
         )
     }
 }
