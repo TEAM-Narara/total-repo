7dcad9eaec2a6938c5b79d671f9055eb42258f86
skarudals27 <skarudals27@naver.com>
2024-10-24 18:02:17 +0900

    [BE] ✨ feat : 스프링 시큐리티 Config 설정

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/config/security/SecurityConfiguration.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/config/security/SecurityConfiguration.java
@@ -5,25 +5,56 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 /**
  * Spring Security 설정
  */
 @EnableWebSecurity(debug = true) // Spring Security사용해 웹 보안 구성시 사용
 @Configuration
 public class SecurityConfiguration {
+
+    // /error, /favicon.ico에 대한 경로 열어주지 않으면 401로 계속 뜸
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
+        return web -> web.ignoring()
+                // error endpoint를 열어줘야 함, favicon.ico 추가!
+                .requestMatchers("/error", "/favicon.ico");
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //jwt 사용
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화 -> cookie를 사용하지 않으면 꺼도 된다. (cookie를 사용할 경우 httpOnly(XSS 방어), sameSite(CSRF 방어)로 방어해야 한다.)
+                .cors(AbstractHttpConfigurer::disable) // cors 비활성화 -> 프론트와 연결 시 따로 설정 필요
+                .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 login form 비활성화
+                .logout(AbstractHttpConfigurer::disable) // 기본 logout 비활성화
+                .headers(c -> c.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::disable).disable()) // X-Frame-Options 비활성화
+                .sessionManagement(c ->
+                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용하지 않음
+
+                // request 인증, 인가 설정
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
-                );
-                // .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // jwt 필터 추가
+                )
+                // jwt 관련 설정
+                .addFilterBefore(JwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationFilter(), JwtAuthenticationFilter.getClass()) // 토큰 예외 핸들링
+
+                // 인증 예외 핸들링
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                        .accessDeniedHandler(new CustomAccessDeniedHandler()));
+
         return http.build();
     }
 }
