aaa485b44f8b1297356cf4400cf02ca156e3149d
정다혜 <hyejjang1203@naver.com>
2024-10-24 15:53:22 +0900

    [FE] 💄 design: Update Profile Navigation 연결

--- Android/app/src/main/java/com/ssafy/superboard/navigation/SuperBoardDestination.kt
+++ Android/app/src/main/java/com/ssafy/superboard/navigation/SuperBoardDestination.kt
@@ -4,10 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.ssafy.login.login.LogIn
-import com.ssafy.login.login.loginScreen
-import com.ssafy.login.signup.SignUp
-import com.ssafy.login.signup.signupScreen
 import com.ssafy.board.boardScreen
 import com.ssafy.home.createboard.CreateBoard
 import com.ssafy.home.createboard.createBoardScreen
@@ -17,10 +13,16 @@ import com.ssafy.home.mycard.MyCard
 import com.ssafy.home.mycard.myCardScreen
 import com.ssafy.home.setting.Setting
 import com.ssafy.home.setting.settingScreen
+import com.ssafy.home.update.UpdateProfile
+import com.ssafy.home.update.updateProfileScreen
+import com.ssafy.login.login.LogIn
+import com.ssafy.login.login.loginScreen
+import com.ssafy.login.signup.SignUp
+import com.ssafy.login.signup.signupScreen
 
 @Composable
 fun SuperBoardNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
-    NavHost(navController = navController, startDestination = LogIn, modifier = modifier) {
+    NavHost(navController = navController, startDestination = Home, modifier = modifier) {
         loginScreen(moveToSignUpScreen = {
             navController.navigate(SignUp)
         })
@@ -55,6 +57,9 @@ fun SuperBoardNavHost(navController: NavHostController, modifier: Modifier = Mod
             },
             moveToMyCardScreen = {
                 navController.navigate(MyCard)
+            },
+            moveToUpdateProfile = {
+                navController.navigate(UpdateProfile)
             }
         )
 
@@ -85,5 +90,7 @@ fun SuperBoardNavHost(navController: NavHostController, modifier: Modifier = Mod
         boardScreen(popBack = {
             navController.popBackStack()
         })
+
+        updateProfileScreen(backHomeScreen = { navController.popBackStack() })
     }
 }
