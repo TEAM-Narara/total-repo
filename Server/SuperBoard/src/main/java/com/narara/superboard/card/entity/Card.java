db4c41bd16436a6aa8c79a8b1baf82d6cab6d767
조시현 <si4018@naver.com>
2024-10-25 15:55:38 +0900

    [BE] ✨ feat: 카드 엔티티 생성

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/entity/Card.java
@@ -0,0 +1,54 @@
+package com.narara.superboard.card.entity;
+
+import com.narara.superboard.list.entity.List;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+
+import java.util.Map;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @JoinColumn(name = "list_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private List list;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_at")
+    private Long startAt; // Assuming this is a Unix timestamp or epoch time
+
+    @Column(name = "end_at")
+    private Long endAt; // Assuming this is a Unix timestamp or epoch time
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "cover", columnDefinition = "jsonb")
+    private Map<String, Object> cover;
+
+    @Column(name = "my_order", nullable = false, columnDefinition = "bigint default 0")
+    private Long myOrder;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isDeleted = false;
+
+    @Column(name = "is_archived", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isArchived = false;
+}
+
