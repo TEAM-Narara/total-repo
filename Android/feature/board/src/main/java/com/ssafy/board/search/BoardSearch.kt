bd225a6f29258840ecd190cf80731de9d6fc71ea
최승준 <csjune99@naver.com>
2024-10-25 15:43:32 +0900

    [FE] 💄 design: 보드 내부 검색 화면 구현

---
+++ Android/feature/board/src/main/java/com/ssafy/board/search/BoardSearch.kt
@@ -0,0 +1,156 @@
+package com.ssafy.board.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ssafy.board.search.dto.SearchAllParameters
+import com.ssafy.designsystem.component.CheckBoxGroup
+import com.ssafy.designsystem.component.EditableText
+import com.ssafy.designsystem.component.IconButton
+import com.ssafy.designsystem.values.IconMedium
+import com.ssafy.designsystem.values.PaddingDefault
+import com.ssafy.model.search.DueDate
+import com.ssafy.model.search.Label
+import com.ssafy.model.search.SearchParameters
+
+@Composable
+fun BoardSearchScreen(
+    viewModel: BoardSearchViewModel = hiltViewModel(),
+    popBackToBoardScreenWithParams: (SearchParameters) -> Unit,
+    popBackToBoardScreen: () -> Unit
+) {
+    val parameters by viewModel.searchAllParameters.collectAsStateWithLifecycle()
+
+    BoardSearchScreen(
+        parameters = parameters,
+        updateSearchText = viewModel::updateSearchText,
+        updateMember = viewModel::updateMember,
+        updateDueDate = viewModel::updateDueDate,
+        updateLabel = viewModel::updateLabel,
+        popBackToBoardScreen = { popBackToBoardScreen() },
+        popBackToBoardScreenWithParams = { popBackToBoardScreenWithParams(viewModel.getSearchParameters()) }
+    )
+}
+
+@Composable
+private fun BoardSearchScreen(
+    parameters: SearchAllParameters,
+    updateSearchText: (String) -> Unit,
+    updateMember: (String) -> Unit,
+    updateDueDate: (DueDate) -> Unit,
+    updateLabel: (Label) -> Unit,
+    popBackToBoardScreenWithParams: () -> Unit,
+    popBackToBoardScreen: () -> Unit
+) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(PaddingDefault)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+
+            IconButton(
+                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                modifier = Modifier.size(IconMedium),
+                contentDescription = "뒤로 아이콘",
+                onClick = popBackToBoardScreen
+            )
+
+            EditableText(
+                text = parameters.searchedText,
+                onTextChanged = updateSearchText,
+                modifier = Modifier.weight(1f),
+                maxTitleLength = 40,
+            )
+
+            IconButton(
+                imageVector = Icons.Default.Search,
+                contentDescription = "검색",
+                onClick = popBackToBoardScreenWithParams
+            )
+
+        }
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            color = Color.Gray
+        )
+
+        Spacer(modifier = Modifier.height(PaddingDefault))
+
+        CheckBoxGroup(
+            title = "담당자",
+            options = parameters.memberMap.toList(),
+            onOptionSelected = { updateMember(it.first) },
+            isOptionChecked = { it.second.isSelected },
+            option = { (member, memberInfo) ->
+                OptionText(
+                    startIcon = memberInfo.startIcon,
+                    content = member,
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(PaddingDefault))
+
+        CheckBoxGroup(
+            title = "마감 기한",
+            options = parameters.dueDateMap.toList(),
+            onOptionSelected = { updateDueDate(it.first) },
+            isOptionChecked = { it.second.isSelected },
+            option = { (dueDate, dueDateInfo) ->
+                OptionText(
+                    startIcon = dueDateInfo.startIcon,
+                    content = dueDate.toString(),
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(PaddingDefault))
+
+        CheckBoxGroup(
+            title = "라벨",
+            options = parameters.labelMap.toList(),
+            onOptionSelected = { updateLabel(it.first) },
+            isOptionChecked = { it.second.isSelected },
+            option = { (label, labelInfo) ->
+                OptionText(
+                    startIcon = labelInfo.startIcon,
+                    content = label.content,
+                    backGroundColor = Color(label.color)
+                )
+            }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun BoardSearchScreenPreview() {
+    BoardSearchScreen(
+        popBackToBoardScreen = {},
+        popBackToBoardScreenWithParams = {},
+    )
+}
---
+++ Android/feature/board/src/main/java/com/ssafy/board/search/BoardSearchViewModel.kt
@@ -0,0 +1,123 @@
+package com.ssafy.board.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.board.search.dto.SearchAllParameters
+import com.ssafy.model.search.DueDate
+import com.ssafy.model.search.Label
+import com.ssafy.model.search.SearchParameters
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BoardSearchViewModel @Inject constructor() : ViewModel() {
+
+    private val _searchAllParameters = MutableStateFlow(SearchAllParameters())
+    val searchAllParameters = _searchAllParameters.asStateFlow()
+
+    fun updateDueDate(dueDate: DueDate) = viewModelScope.launch {
+        val dueDateMap = searchAllParameters.value.dueDateMap.toMutableMap()
+
+        when (dueDate) {
+            DueDate.NO_DUE_DATE, DueDate.OVERDUE -> {
+                dueDateMap[dueDate]?.let {
+                    dueDateMap[dueDate] = it.copy(isSelected = !it.isSelected)
+                }
+            }
+
+            DueDate.DUE_IN_THE_NEXT_DAY -> {
+                dueDateMap[dueDate]?.let { paramInfo ->
+                    dueDateMap[dueDate] = paramInfo.copy(isSelected = !paramInfo.isSelected).apply {
+
+                        if (!isSelected) return@let
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_WEEK]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_WEEK] = it.copy(isSelected = false)
+                        }
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_MONTH]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_MONTH] = it.copy(isSelected = false)
+                        }
+                    }
+                }
+            }
+
+            DueDate.DUE_IN_THE_NEXT_WEEK -> {
+                dueDateMap[dueDate]?.let { paramInfo ->
+                    dueDateMap[dueDate] = paramInfo.copy(isSelected = !paramInfo.isSelected).apply {
+
+                        if (!isSelected) return@let
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_DAY]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_DAY] = it.copy(isSelected = false)
+                        }
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_MONTH]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_MONTH] = it.copy(isSelected = false)
+                        }
+                    }
+                }
+            }
+
+            DueDate.DUE_IN_THE_NEXT_MONTH -> {
+                dueDateMap[dueDate]?.let { paramInfo ->
+                    dueDateMap[dueDate] = paramInfo.copy(isSelected = !paramInfo.isSelected).apply {
+
+                        if (!isSelected) return@let
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_DAY]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_DAY] = it.copy(isSelected = false)
+                        }
+                        dueDateMap[DueDate.DUE_IN_THE_NEXT_WEEK]?.let {
+                            dueDateMap[DueDate.DUE_IN_THE_NEXT_WEEK] = it.copy(isSelected = false)
+                        }
+                    }
+                }
+            }
+        }
+
+        _searchAllParameters.emit(searchAllParameters.value.copy(dueDateMap = dueDateMap))
+    }
+
+    fun updateMember(member: String) = viewModelScope.launch(Dispatchers.IO) {
+        val memberMap = searchAllParameters.value.memberMap.toMutableMap()
+        memberMap[member]?.let {
+            memberMap[member] = it.copy(isSelected = !it.isSelected)
+        }
+        _searchAllParameters.emit(searchAllParameters.value.copy(memberMap = memberMap))
+    }
+
+    fun updateLabel(label: Label) = viewModelScope.launch(Dispatchers.IO) {
+        val labelMap = searchAllParameters.value.labelMap.toMutableMap()
+        labelMap[label]?.let {
+            labelMap[label] = it.copy(isSelected = !it.isSelected)
+        }
+        _searchAllParameters.emit(searchAllParameters.value.copy(labelMap = labelMap))
+    }
+
+    fun updateSearchText(searchText: String) {
+        searchAllParameters.value.searchedText = searchText
+    }
+
+    fun updateSearchParams(params: SearchParameters) = viewModelScope.launch(Dispatchers.IO) {
+        with(params) {
+            dueDates.forEach { updateDueDate(it) }
+            members.forEach { updateMember(it) }
+            labels.forEach { updateLabel(it) }
+            searchAllParameters.value.searchedText = searchText
+        }
+    }
+
+    fun getSearchParameters(): SearchParameters {
+        val dueDates = searchAllParameters.value.dueDateMap.filter { it.value.isSelected }.keys
+        val members = searchAllParameters.value.memberMap.filter { it.value.isSelected }.keys
+        val labels = searchAllParameters.value.labelMap.filter { it.value.isSelected }.keys
+        val searchText = searchAllParameters.value.searchedText
+
+        return SearchParameters(
+            searchText = searchText,
+            members = members.toSet(),
+            dueDates = dueDates.toSet(),
+            labels = labels.toSet()
+        )
+    }
+}
