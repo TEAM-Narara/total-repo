f83142815901a5e725f5053f5acae7db14eab3d4
조시현 <si4018@naver.com>
2024-10-25 17:04:22 +0900

    [BE] ♻️ refactor: 메서드 이름 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardService.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardService.java
@@ -4,5 +4,5 @@ import com.narara.superboard.card.entity.Card;
 import com.narara.superboard.card.interfaces.dto.CardCreateRequestDto;
 
 public interface CardService {
-    Card creadCard(CardCreateRequestDto cardCreateRequestDto);
+    Card createCard(CardCreateRequestDto cardCreateRequestDto);
 }
