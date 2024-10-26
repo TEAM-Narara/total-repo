3220f030220de0a25f815e2ac1ece33de74758fc
정다혜 <hyejjang1203@naver.com>
2024-10-26 21:27:08 +0900

    [FE] 💄 design: BackArrow 아이콘 deprecate되지 않은 버전으로 변경

--- Android/feature/home/src/main/java/com/ssafy/home/update/UpdateProfileScreen.kt
+++ Android/feature/home/src/main/java/com/ssafy/home/update/UpdateProfileScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -65,7 +65,7 @@ fun UpdateProfileScreen(
                 navigationIcon = {
                     IconButton(onClick = onBackPressed) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "",
                             modifier = Modifier.size(IconMedium)
                         )
