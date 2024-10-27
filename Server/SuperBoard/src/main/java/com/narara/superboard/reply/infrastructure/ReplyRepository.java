8358ee10042a579ab939eb4872d66572435758cc
sihyunjojo <si4018@naver.com>
2024-10-27 20:20:55 +0900

    [BE] ✨ feat: 카드의 댓글에 대한 조회 구현

--- Server/SuperBoard/src/main/java/com/narara/superboard/reply/infrastructure/ReplyRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/reply/infrastructure/ReplyRepository.java
@@ -1,7 +1,10 @@
 package com.narara.superboard.reply.infrastructure;
 
+import com.narara.superboard.card.entity.Card;
 import com.narara.superboard.reply.entity.Reply;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findAllByCard(Card card);
 }
