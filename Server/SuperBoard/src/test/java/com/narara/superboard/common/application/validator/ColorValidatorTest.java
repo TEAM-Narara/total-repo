c9e70db74618af7e9e1f5c67f0b645e7137db586
조시현 <si4018@naver.com>
2024-10-28 11:22:23 +0900

    [BE] ✅ test: 색상이 부적절한 값이 들어오면 예외 발생 테스트 구현

--- Server/SuperBoard/src/test/java/com/narara/superboard/common/application/validator/ColorValidatorTest.java
+++ Server/SuperBoard/src/test/java/com/narara/superboard/common/application/validator/ColorValidatorTest.java
@@ -1,5 +1,6 @@
 package com.narara.superboard.common.application.validator;
 
+import com.narara.superboard.common.exception.color.InvalidColorFormatException;
 import com.narara.superboard.common.exception.color.NotFoundColorException;
 import com.narara.superboard.common.interfaces.dto.ColorHolder;
 import org.junit.jupiter.api.BeforeEach;
@@ -43,5 +44,18 @@ class ColorValidatorTest {
 
         // then
         assertDoesNotThrow(() -> colorValidator.validateColorIsEmpty(colorHolder.color(),entity));
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {0xFFFFFFFFL + 1, -1L, 0xFFFFFFFFFFL})
+    @DisplayName("라벨 컬러가 유효 범위를 벗어날 때 InvalidColorFormatException 발생")
+    void shouldThrowExceptionWhenColorIsOutOfRange(Long color) {
+        // given
+        String entity = "TestEntity";
+        ColorHolder colorHolder = mock(ColorHolder.class);
+        when(colorHolder.color()).thenReturn(color);
+
+        // then
+        assertThrows(InvalidColorFormatException.class, () -> colorValidator.validateColorIsValid(colorHolder.color(),entity));
     }
 }
