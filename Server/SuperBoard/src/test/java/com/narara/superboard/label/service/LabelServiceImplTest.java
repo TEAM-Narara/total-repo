f4b395c858289823ac57148ed0402954d958042a
조시현 <si4018@naver.com>
2024-10-28 11:17:15 +0900

    [BE] ✅ test: 색상이 빈값일 때, 예외가 발생한다.

---
+++ Server/SuperBoard/src/test/java/com/narara/superboard/label/service/LabelServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.narara.superboard.label.service;
+
+import com.narara.superboard.MockSuperBoardUnitTests;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("라벨 서비스에 대한 단위 테스트")
+class LabelServiceImplTest implements MockSuperBoardUnitTests {
+
+    @InjectMocks
+    private LabelServiceImpl labelService;
+
+}
