76899deda4afe829847004cbc879070b88206be3
조시현 <si4018@naver.com>
2024-10-28 11:23:00 +0900

    [BE] ✨ feat: 적절 하지 않은 색상이 들어오면 예외 발생 로직 구현

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/application/validator/ColorValidator.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/application/validator/ColorValidator.java
@@ -1,10 +1,18 @@
 package com.narara.superboard.common.application.validator;
 
+import com.narara.superboard.common.exception.color.InvalidColorFormatException;
 import com.narara.superboard.common.exception.color.NotFoundColorException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ColorValidator {
+
+    public void validateColorIsValid(Long color, String entity) {
+        // color 값이 0x00000000 ~ 0xFFFFFFFF 범위에 있는지 확인
+        if (color < 0x00000000L || color > 0xFFFFFFFFL) {
+            throw new InvalidColorFormatException(entity);
+        }
+    }
 
     public void validateColorIsEmpty(Long color, String entity) {
         if (color == null) {
