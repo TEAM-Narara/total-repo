3508706a68f7b5eaadb86e40b1734b84eed06a12
조시현 <si4018@naver.com>
2024-10-28 17:07:28 +0900

    [BE] ♻️ refactor: static import 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardRepository.java
@@ -1,9 +1,9 @@
 package com.narara.superboard.card.infrastructure;
 
 import com.narara.superboard.card.entity.Card;
-import java.util.List;
+import com.narara.superboard.list.entity.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
-    List<Card> findAllByListAndIsArchivedTrue(com.narara.superboard.list.entity.List list);
+    java.util.List<Card> findAllByListAndIsArchivedTrue(List list);
 }
