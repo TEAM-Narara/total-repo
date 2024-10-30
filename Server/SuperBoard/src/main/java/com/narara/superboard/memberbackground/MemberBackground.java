6dd877e896bfe5e069496076506b2fd91a1653d7
조시현 <si4018@naver.com>
2024-10-30 15:12:36 +0900

    [BE] ✨ feat: 회원 배경에 대한 엔티티 생성

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/memberbackground/MemberBackground.java
@@ -0,0 +1,25 @@
+package com.narara.superboard.memberbackground;
+
+import com.narara.superboard.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "member_background")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberBackground {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String url;
+}
