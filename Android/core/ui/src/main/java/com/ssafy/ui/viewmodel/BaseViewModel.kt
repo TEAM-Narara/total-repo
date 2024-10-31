98b9320371adf5fc441802a558216ed6d424a0db
최승준 <csjune99@naver.com>
2024-10-31 22:45:20 +0900

    [FE] ✨ feat: 로딩에 강제 시간 할당...

--- Android/core/ui/src/main/java/com/ssafy/ui/viewmodel/BaseViewModel.kt
+++ Android/core/ui/src/main/java/com/ssafy/ui/viewmodel/BaseViewModel.kt
@@ -2,6 +2,7 @@ package com.ssafy.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.ssafy.ui.uistate.UiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,9 +17,12 @@ open class BaseViewModel : ViewModel() {
 
     fun <T> Flow<T>.withUiState() = this.onStart {
         _uiState.update { UiState.Loading }
-    }.onCompletion {
-        _uiState.update { UiState.Success }
+        // 너무 빨리 로딩이 끝나면 collectLatest이기 때문에 로딩 상태를 감지하지 못함
+        delay(100)
+    }.onCompletion { cause ->
+        if (cause == null) _uiState.update { UiState.Success }
     }.catch { exception ->
+        exception.printStackTrace()
         _uiState.update { UiState.Error(exception.message) }
     }
 
