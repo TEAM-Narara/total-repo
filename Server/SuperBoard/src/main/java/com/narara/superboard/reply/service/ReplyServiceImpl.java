bb4063f1fb4aa9cf0736414afa052edc8edde9f8
sihyunjojo <si4018@naver.com>
2024-11-01 01:52:45 +0900

    [BE] ✨ feat: 댓글 권한 체크 로직 구현

--- Server/SuperBoard/src/main/java/com/narara/superboard/reply/service/ReplyServiceImpl.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/reply/service/ReplyServiceImpl.java
@@ -1,8 +1,10 @@
 package com.narara.superboard.reply.service;
 
 import com.narara.superboard.boardmember.entity.BoardMember;
+import com.narara.superboard.card.CardAction;
 import com.narara.superboard.card.entity.Card;
 import com.narara.superboard.card.infrastructure.CardRepository;
+import com.narara.superboard.card.service.CardService;
 import com.narara.superboard.common.application.validator.ContentValidator;
 import com.narara.superboard.common.exception.DeletedEntityException;
 import com.narara.superboard.common.exception.NotFoundEntityException;
@@ -24,6 +26,8 @@ import static com.narara.superboard.websocket.enums.ReplyAction.EDIT_REPLY;
 @RequiredArgsConstructor
 public class ReplyServiceImpl implements ReplyService{
 
+    private final CardService cardService;
+
     private final ReplyRepository replyRepository;
     private final CardRepository cardRepository;
 
@@ -36,7 +40,7 @@ public class ReplyServiceImpl implements ReplyService{
         Card card = cardRepository.findById(replyCreateRequestDto.cardId())
                 .orElseThrow(() -> new NotFoundEntityException(replyCreateRequestDto.cardId(), "카드"));
 
-//        TODO: 댓글을 포함하는 보드의 권한이 있는지 확인
+        cardService.checkBoardMember(card,member, CardAction.ADD_CARD);
 
         Reply reply = Reply.createReply(replyCreateRequestDto, card, member);
 
@@ -45,7 +49,6 @@ public class ReplyServiceImpl implements ReplyService{
 
     @Override
     public Reply getReply(Long replyId) {
-//        TODO: 댓글을 포함하는 보드의 권한이 있는지 확인
         return replyRepository.findById(replyId)
                 .orElseThrow(() -> new NotFoundEntityException(replyId, "댓글"));
     }
