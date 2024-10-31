f4f4a037b83c90b73bf2d9b51277955320b9536f
조시현 <si4018@naver.com>
2024-10-31 13:02:04 +0900

    [BE] ✨ feat: 댓글 수정 삭제 endpoint 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/reply/interfaces/ReplyController.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/reply/interfaces/ReplyController.java
@@ -4,9 +4,12 @@ import com.narara.superboard.member.entity.Member;
 import com.narara.superboard.reply.entity.Reply;
 import com.narara.superboard.reply.interfaces.dto.ReplyCreateRequestDto;
 import com.narara.superboard.reply.interfaces.dto.ReplySimpleResponseDto;
+import com.narara.superboard.reply.interfaces.dto.ReplyUpdateRequestDto;
 import com.narara.superboard.reply.service.ReplyService;
+import com.narara.superboard.websocket.enums.ReplyAction;
 import com.narara.superboard.workspace.interfaces.dto.websocket.WebSocketResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,7 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import static com.narara.superboard.websocket.enums.ReplyAction.ADD_REPLY;
+import static com.narara.superboard.websocket.enums.ReplyAction.*;
 
 @CrossOrigin
 @Controller
@@ -29,9 +32,29 @@ public class ReplyController implements ReplyDestination {
 
     @Override
     @MessageMapping("/create")
-    public WebSocketResponse createReply(@AuthenticationPrincipal Member member,@RequestBody ReplyCreateRequestDto replyCreateRequestDto) {
+    public WebSocketResponse createReply(@AuthenticationPrincipal Member member, @RequestBody ReplyCreateRequestDto replyCreateRequestDto) {
         Reply reply = replyService.createReply(member, replyCreateRequestDto);
 
+        return getWebSocketResponse(reply, ADD_REPLY, "/topic/board/");
+    }
+
+    @MessageMapping("/update/{cardId}")
+    public WebSocketResponse updateReply(@DestinationVariable Long cardId, @RequestBody ReplyUpdateRequestDto replyUpdateRequestDto, @AuthenticationPrincipal Member member) {
+        // TODO: 멤버 권한 체크하기.
+        Reply reply = replyService.updateReply(member, cardId, replyUpdateRequestDto);
+
+        return getWebSocketResponse(reply, EDIT_REPLY, "/topic/board/");
+    }
+
+
+    @MessageMapping("/delete/{cardId}")
+    public WebSocketResponse deleteReply(@DestinationVariable Long cardId, @AuthenticationPrincipal Member member) {
+        Reply reply = replyService.deleteReply(member, cardId);
+
+        return getWebSocketResponse(reply, DELETE_REPLY, "/topic/reply/");
+    }
+
+    private WebSocketResponse getWebSocketResponse(Reply reply, ReplyAction action, String x) {
         ReplySimpleResponseDto cardDto = ReplySimpleResponseDto.builder()
                 .cardId(reply.getCard().getId())
                 .replyId(reply.getId())
@@ -39,12 +62,11 @@ public class ReplyController implements ReplyDestination {
                 .build();
 
         // WebSocket 응답 객체 생성
-        WebSocketResponse response = WebSocketResponse.of(REPLY, ADD_REPLY, cardDto);
+        WebSocketResponse response = WebSocketResponse.of(REPLY, action, cardDto);
 
         // 동적 경로로 메시지 전송
         Long boardId = reply.getCard().getList().getBoard().getId();
-        messagingTemplate.convertAndSend("/topic/reply/" + boardId, response);
-
+        messagingTemplate.convertAndSend(x + boardId, response);
         return response;
     }
 }
