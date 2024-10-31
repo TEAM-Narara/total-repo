1c208f28c6060fc12db07060e6eaf952cd4d256b
조시현 <si4018@naver.com>
2024-10-31 14:05:54 +0900

    [BE] ✨ feat: 멤버에 대한 댓글 여부 확인 로직 구현

--- Server/SuperBoard/src/main/java/com/narara/superboard/replymember/infrastructure/ReplyMemberRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/replymember/infrastructure/ReplyMemberRepository.java
@@ -1,7 +1,10 @@
 package com.narara.superboard.replymember.infrastructure;
 
+import com.narara.superboard.member.entity.Member;
+import com.narara.superboard.reply.entity.Reply;
 import com.narara.superboard.replymember.entity.ReplyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyMemberRepository extends JpaRepository<ReplyMember, Long> {
+    Boolean existsByMemberAndReply(Member member, Reply reply);
 }
