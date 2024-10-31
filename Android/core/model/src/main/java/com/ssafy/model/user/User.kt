682068ba670723d9c1e34320435e3e2dc6d0beac
최승준 <csjune99@naver.com>
2024-10-31 22:40:39 +0900

    [FE] ✨ feat: 로그인 DTO 및 user DTO 구현

--- Android/core/model/src/main/java/com/ssafy/model/user/User.kt
+++ Android/core/model/src/main/java/com/ssafy/model/user/User.kt
@@ -3,6 +3,7 @@ package com.ssafy.model.user
 data class User(
     val id: Long = 0L,
     val email: String,
-    val nickname: String,
-    val profileImage: String?
+    val nickname: String = "",
+    val password: String? = null,
+    val profileImage: String? = null,
 )
