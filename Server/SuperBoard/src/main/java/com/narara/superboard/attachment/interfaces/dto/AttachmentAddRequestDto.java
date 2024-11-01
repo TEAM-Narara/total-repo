1493399c1b3c20988427a9ab5a2df715131ef792
skarudals27 <skarudals27@naver.com>
2024-11-01 13:26:57 +0900

    [BE] :zap: feat : 첨부파일 관련 설정

--- Server/SuperBoard/src/main/java/com/narara/superboard/attachment/interfaces/dto/AttachmentAddRequestDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/attachment/interfaces/dto/AttachmentAddRequestDto.java
@@ -1,6 +1,5 @@
 package com.narara.superboard.attachment.interfaces.dto;
 
-public record AttachmentAddRequestDto(Long cardId,String url,String type) {
+public record AttachmentAddRequestDto(Long cardId,String url) {
 
-    // 커버가 null이면 첨부파일 넣어주기
 }
