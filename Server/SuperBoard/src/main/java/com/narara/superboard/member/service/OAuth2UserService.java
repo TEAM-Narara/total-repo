0a10aa6756f79807dfeb69ed4737ca18e9380025
skarudals27 <skarudals27@naver.com>
2024-11-01 15:17:09 +0900

    [BE] :bug: fix: 소셜 로그인 수정

--- Server/SuperBoard/src/main/java/com/narara/superboard/member/service/OAuth2UserService.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/member/service/OAuth2UserService.java
@@ -1,8 +1,9 @@
 package com.narara.superboard.member.service;
 
 import com.narara.superboard.member.entity.Member;
+import com.narara.superboard.member.interfaces.dto.MemberLoginResponseDto;
 import com.narara.superboard.member.interfaces.dto.TokenDto;
 
 public interface OAuth2UserService {
-    TokenDto getUserInfo(String accessToken, String provider);
+    MemberLoginResponseDto getUserInfo(String accessToken, String provider);
 }
