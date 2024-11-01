585a521b20171c74956046aca22648b0a65179d7
조시현 <si4018@naver.com>
2024-11-01 12:28:02 +0900

    [BE] ✨ feat: 생성 메서드 로직 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardDetailResponseDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardDetailResponseDto.java
@@ -1,5 +1,7 @@
 package com.narara.superboard.board.interfaces.dto;
 
+import com.narara.superboard.board.entity.Board;
+import com.narara.superboard.common.application.handler.CoverHandler;
 import lombok.Builder;
 
 @Builder
@@ -12,4 +14,15 @@ public record BoardDetailResponseDto(
         String visibility,
         Boolean isClosed
 ) {
+    public static BoardDetailResponseDto of(Board board, CoverHandler coverHandler) {
+        return BoardDetailResponseDto.builder()
+                .id(board.getId())
+                .workspaceId(board.getWorkSpace().getId())
+                .name(board.getName())
+                .backgroundType(coverHandler.getTypeValue(board.getCover()))
+                .backgroundValue(coverHandler.getValue(board.getCover()))
+                .visibility(board.getVisibility().name())
+                .isClosed(board.getIsArchived())
+                .build();
+    }
 }
--- Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardSimpleResponseDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/interfaces/dto/BoardSimpleResponseDto.java
@@ -1,5 +1,10 @@
 package com.narara.superboard.board.interfaces.dto;
 
+import com.narara.superboard.board.entity.Board;
+import com.narara.superboard.common.application.handler.CoverHandler;
+import lombok.Builder;
+
+@Builder
 public record BoardSimpleResponseDto(
         Long id,
         Long workspaceId,
@@ -8,4 +13,14 @@ public record BoardSimpleResponseDto(
         String backgroundValue,
         Boolean isClosed
 ) {
+    public static BoardSimpleResponseDto of(Board board, CoverHandler coverHandler) {
+       return BoardSimpleResponseDto.builder()
+               .id(board.getId())
+               .workspaceId(board.getWorkSpace().getId())
+               .name(board.getName())
+               .backgroundType(coverHandler.getTypeValue(board.getCover()))
+               .backgroundValue(coverHandler.getValue(board.getCover()))
+               .isClosed(board.getIsArchived())
+               .build();
+    }
 }
