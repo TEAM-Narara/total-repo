c1a8ca13f2397f51e95e4e7bfa871483adb48a2c
최승준 <csjune99@naver.com>
2024-11-01 20:36:36 +0900

    [FE] ✨ feat: 회원 가입 시 유저 정보 저장

--- Android/domain/login/src/main/java/com/ssafy/login/RegisterUseCase.kt
+++ Android/domain/login/src/main/java/com/ssafy/login/RegisterUseCase.kt
@@ -1,17 +1,22 @@
 package com.ssafy.login
 
 import com.ssafy.data.repository.user.UserRepository
+import com.ssafy.datastore.DataStoreRepository
 import com.ssafy.model.user.User
 import com.ssafy.model.user.signup.RegisterDTO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RegisterUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val dataStoreRepository: DataStoreRepository
 ) {
 
-    suspend operator fun invoke(registerDTO: RegisterDTO): Flow<User> {
-        return userRepository.register(registerDTO)
+    suspend operator fun invoke(registerDTO: RegisterDTO): Flow<Unit> {
+        return userRepository.register(registerDTO).map { user: User ->
+            dataStoreRepository.saveUser(user)
+        }
     }
 
 }
