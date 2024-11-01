96d25005505081a624e209934be2ff6b9dec8282
조시현 <si4018@naver.com>
2024-11-01 11:11:04 +0900

    [BE] ✨ feat: 리스트 restapi 추가

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/list/interfaces/ListAPI.java
@@ -0,0 +1,26 @@
+package com.narara.superboard.list.interfaces;
+
+import com.narara.superboard.common.interfaces.response.DefaultResponse;
+import com.narara.superboard.list.interfaces.dto.ListCreateRequestDto;
+import com.narara.superboard.list.interfaces.dto.ListSimpleResponseDto;
+import com.narara.superboard.list.interfaces.dto.ListUpdateRequestDto;
+import com.narara.superboard.member.entity.Member;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/v1/list")
+public interface ListAPI {
+
+    @PostMapping("/")
+    ResponseEntity<DefaultResponse<ListSimpleResponseDto>> createList(@AuthenticationPrincipal Member member, @RequestBody ListCreateRequestDto listCreateRequestDto);
+
+    @PatchMapping("/{listId}")
+    ResponseEntity<DefaultResponse<ListSimpleResponseDto>> updateList(@AuthenticationPrincipal Member member, @PathVariable Long listId, @RequestBody ListUpdateRequestDto listUpdateRequestDto);
+
+    @PatchMapping("/{listId}/archive")
+    ResponseEntity<DefaultResponse<ListSimpleResponseDto>> changeListIsArchived(@AuthenticationPrincipal Member member, @PathVariable Long listId);
+
+    @GetMapping("/{boardId}/archived")
+    ResponseEntity<DefaultResponse<java.util.List<ListSimpleResponseDto>>> getArchivedList(@AuthenticationPrincipal Member member, @PathVariable Long boardId);
+}
