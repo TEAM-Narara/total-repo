6d8c7623cfa9ab5e1d0c09924237d1ae353306e1
sihyunjojo <si4018@naver.com>
2024-11-04 00:32:52 +0900

    [BE] ♻️ refactor: url 형식 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/boardmember/interfaces/BoardMemberAPI.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/boardmember/interfaces/BoardMemberAPI.java
@@ -14,24 +14,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "보드 멤버 API", description = "보드 멤버 관련 기능을 제공하는 API")
-@RequestMapping("/api/v1/board/{boardId}/members")
+@RequestMapping("/api/v1/boards/{boardId}")
 public interface BoardMemberAPI {
 
     @Operation(summary = "보드 멤버 목록 조회", description = "지정된 보드에 속한 모든 멤버 정보를 조회합니다.")
-    @GetMapping
+    @GetMapping("/members")
     ResponseEntity<DefaultResponse<BoardMemberCollectionResponseDto>> getBoardMembers(
             @Parameter(description = "조회할 보드의 ID", required = true) @PathVariable Long boardId
     );
 
     @Operation(summary = "워치 상태 조회", description = "사용자의 워치(알림) 상태를 조회합니다.")
-    @GetMapping("/watch-status")
+    @GetMapping("/member/watch-status")
     ResponseEntity<DefaultResponse<Boolean>> getWatchStatus(
             @Parameter(description = "조회할 보드의 ID", required = true) @PathVariable Long boardId,
             @AuthenticationPrincipal @Parameter(hidden = true) Member member
     );
 
     @Operation(summary = "워치 상태 변경", description = "사용자의 워치(알림) 상태를 토글합니다.")
-    @PutMapping("/watch-status")
+    @PutMapping("/member/watch-status")
     ResponseEntity<DefaultResponse<Void>> updateWatchStatus(
             @Parameter(description = "변경할 보드의 ID", required = true) @PathVariable Long boardId,
             @AuthenticationPrincipal @Parameter(hidden = true) Member member
