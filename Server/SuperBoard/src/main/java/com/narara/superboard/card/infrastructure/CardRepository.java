f63ce89c1721540f8b56cb20450b31ed0667f860
namgyeongmin <skarudals27@naver.com>
2024-11-04 01:11:25 +0900

    [BE] ✨ feat: 카드 라벨 조회(전체 라벨 + 카드별 라벨)

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardRepository.java
@@ -1,9 +1,16 @@
 package com.narara.superboard.card.infrastructure;
 
+import com.narara.superboard.board.entity.Board;
 import com.narara.superboard.card.entity.Card;
 import com.narara.superboard.list.entity.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     java.util.List<Card> findAllByListAndIsArchivedTrue(List list);
+
+    // cardId로 보드 엔티티 전체 조회
+    @Query("SELECT c.list.board FROM Card c WHERE c.id = :cardId")
+    Board findBoardByCardId(@Param("cardId") Long cardId);
 }
--- Server/SuperBoard/src/main/java/com/narara/superboard/cardlabel/service/CardLabelService.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/cardlabel/service/CardLabelService.java
@@ -10,8 +10,7 @@ import java.util.List;
 public interface CardLabelService {
     CardLabel createCardLabel(Card card, Label label);
     CardLabel changeCardLabelIsActivated(Card card, Label label);
-    List<CardLabel> getCardLabels(Card card);
 
     // TODO : 카드 라벨 조회 (보드의 라벨도 포함)
-    List<CardLabelDto> getCardLabelsDto(Long cardId);
+    List<CardLabelDto> getCardLabelCollection(Long cardId);
 }
