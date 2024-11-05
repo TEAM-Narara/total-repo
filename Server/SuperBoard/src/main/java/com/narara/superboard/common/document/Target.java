5a2afb70de93d22537e61e0c9afa8bd1a2971e2a
조시현 <si4018@naver.com>
2024-11-05 12:53:59 +0900

    [BE] ♻️ refactor: 타입 변경 및 생성 메서드 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/common/document/Target.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/document/Target.java
@@ -1,9 +1,27 @@
 package com.narara.superboard.common.document;
 
+import com.narara.superboard.reply.entity.Reply;
+import lombok.Builder;
+
+
+@Builder
 public record Target(
-        String targetId,
+        Long targetId,
         String targetName,
         AdditionalDetails additionalDetails
 ) {
+    public static Target of(Identifiable identifiable, AdditionalDetails additionalDetails) {
+        return Target.builder()
+                .targetId(identifiable.getId())
+                .targetName(identifiable.getName())
+                .additionalDetails(additionalDetails)
+                .build();
+    }
 
+    public static Target of(Reply reply, AdditionalDetails additionalDetails) {
+        return Target.builder()
+                .targetId(reply.getId())
+                .additionalDetails(additionalDetails)
+                .build();
+    }
 }
--- Server/SuperBoard/src/main/java/com/narara/superboard/common/document/Who.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/common/document/Who.java
@@ -1,7 +1,17 @@
 package com.narara.superboard.common.document;
 
+import com.narara.superboard.member.entity.Member;
+import lombok.Builder;
+
+@Builder
 public record Who(
-        String userId,
-        String userNickname
+        Long memberId,
+        String memberNickname
 ) {
+    public static Who of(Member member) {
+        return Who.builder()
+                .memberId(member.getId())
+                .memberNickname(member.getNickname())
+                .build();
+    }
 }
