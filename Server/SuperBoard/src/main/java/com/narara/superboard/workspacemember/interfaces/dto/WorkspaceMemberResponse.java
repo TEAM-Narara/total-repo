47d0869226e6b3abc44e1ce1c6f69cd36f711b00
lime <joohr1234@gmail.com>
2024-11-05 10:57:41 +0900

    [BE] 🐛 fix: workspace nameHolder 사용하도록 수정

--- Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/interfaces/dto/WorkspaceMemberResponse.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/interfaces/dto/WorkspaceMemberResponse.java
@@ -1,6 +1,7 @@
 package com.narara.superboard.workspacemember.interfaces.dto;
 
 import com.narara.superboard.common.constant.enums.Authority;
+import com.narara.superboard.workspace.interfaces.dto.WorkSpaceNameHolder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class WorkspaceMemberResponse {
+public class WorkspaceMemberResponse implements WorkSpaceNameHolder {
     private Long memberId;
     private Long workspaceId;
     private String memberName;
     private String workspaceName;
     private Authority authority;
+
+    @Override
+    public String name() {
+        return workspaceName;
+    }
 }
