14f0ae9575c27f2aa4c3e3b48295e9d94cc8d3db
조시현 <si4018@naver.com>
2024-11-05 16:10:19 +0900

    [BE] 📝 docs: swagger 재배치

--- Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/interfaces/WorkSpaceMemberAPI.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/workspacemember/interfaces/WorkSpaceMemberAPI.java
@@ -2,10 +2,8 @@ package com.narara.superboard.workspacemember.interfaces;
 
 import com.narara.superboard.common.interfaces.response.DefaultResponse;
 import com.narara.superboard.member.entity.Member;
-import com.narara.superboard.workspace.interfaces.dto.WorkSpaceListResponseDto;
 import com.narara.superboard.workspace.interfaces.dto.WorkSpaceResponseDto;
 import com.narara.superboard.workspacemember.interfaces.dto.WorkSpaceMemberDetailResponseDto;
-import com.narara.superboard.workspacemember.interfaces.dto.WorkspaceMemberCollectionResponseDto;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -17,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Tag(name = "WorkspaceMember", description = "워크스페이스 멤버 관련 API")
+@Tag(name = "워크스페이스 회원", description = "워크스페이스 멤버 관련 API")
 public interface WorkSpaceMemberAPI {
 
     @Operation(summary = "워크스페이스 멤버 조회", description = "워크스페이스 ID를 통해 멤버 리스트를 조회합니다.")
