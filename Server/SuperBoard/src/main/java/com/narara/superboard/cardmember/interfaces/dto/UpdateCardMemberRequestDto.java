83dd345b515a59fe737457a955877c2b5bf1c4ff
skarudals27 <skarudals27@naver.com>
2024-11-05 09:40:46 +0900

    [BE] :zap: feat : 카드 담당자 수정

--- Server/SuperBoard/src/main/java/com/narara/superboard/cardmember/interfaces/dto/UpdateCardMemberRequestDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/cardmember/interfaces/dto/UpdateCardMemberRequestDto.java
@@ -2,5 +2,5 @@ package com.narara.superboard.cardmember.interfaces.dto;
 
 import java.util.List;
 
-public record UpdateCardMemberRequestDto(Long cardId, Long memberId, Boolean is_alert) {
+public record UpdateCardMemberRequestDto(Long cardId, Long memberId) {
 }
