ae46103e401697e6049a36fd3a5767b3549ecbfd
조시현 <si4018@naver.com>
2024-11-05 12:54:10 +0900

    [BE] ♻️ refactor: 댓글 생성 히스토리 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/reply/service/ReplyServiceImpl.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/reply/service/ReplyServiceImpl.java
@@ -2,16 +2,25 @@ package com.narara.superboard.reply.service;
 
 import com.narara.superboard.boardmember.entity.BoardMember;
 import com.narara.superboard.card.CardAction;
+import com.narara.superboard.card.document.CardHistory;
 import com.narara.superboard.card.entity.Card;
+import com.narara.superboard.card.infrastructure.CardHistoryRepository;
 import com.narara.superboard.card.infrastructure.CardRepository;
+import com.narara.superboard.card.service.CardHistoryService;
 import com.narara.superboard.card.service.CardService;
 import com.narara.superboard.common.application.validator.ContentValidator;
+import com.narara.superboard.common.constant.enums.EventData;
+import com.narara.superboard.common.constant.enums.EventType;
+import com.narara.superboard.common.document.AdditionalDetails;
+import com.narara.superboard.common.document.Target;
+import com.narara.superboard.common.document.Who;
 import com.narara.superboard.common.exception.DeletedEntityException;
 import com.narara.superboard.common.exception.NotFoundEntityException;
 import com.narara.superboard.common.exception.authority.UnauthorizedException;
 import com.narara.superboard.member.entity.Member;
 import com.narara.superboard.reply.entity.Reply;
 import com.narara.superboard.reply.infrastructure.ReplyRepository;
+import com.narara.superboard.reply.interfaces.dto.CreateReplyInfo;
 import com.narara.superboard.reply.interfaces.dto.ReplyCreateRequestDto;
 import com.narara.superboard.reply.interfaces.dto.ReplyUpdateRequestDto;
 import com.narara.superboard.websocket.enums.ReplyAction;
@@ -31,6 +40,7 @@ public class ReplyServiceImpl implements ReplyService{
 
     private final ReplyRepository replyRepository;
     private final CardRepository cardRepository;
+    private final CardHistoryRepository cardHistoryRepository;
 
     private final ContentValidator contentValidator;
 
@@ -45,7 +55,19 @@ public class ReplyServiceImpl implements ReplyService{
 
         Reply reply = Reply.createReply(replyCreateRequestDto, card, member);
 
-        return replyRepository.save(reply);
+        Reply savedReply = replyRepository.save(reply);
+
+
+        CreateReplyInfo createReplyInfo = new CreateReplyInfo(reply.getContent());
+        Target target = Target.of(savedReply, createReplyInfo);
+
+        CardHistory cardHistory = CardHistory.careateCardHistory(
+                member, savedReply.getUpdatedAt(), card.getList().getBoard(), card,
+                EventType.CREATE, EventData.COMMENT, target);
+
+        cardHistoryRepository.save(cardHistory);
+
+        return savedReply;
     }
 
     @Override
