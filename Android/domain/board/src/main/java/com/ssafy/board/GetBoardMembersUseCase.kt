86de0894141b7387e4da28f91d9fe964a11823fd
최승준 <csjune99@naver.com>
2024-11-05 19:25:39 +0900

    [FE] ♻️ refactor: DTO 변경!

--- Android/domain/board/src/main/java/com/ssafy/board/GetBoardMembersUseCase.kt
+++ Android/domain/board/src/main/java/com/ssafy/board/GetBoardMembersUseCase.kt
@@ -1,7 +1,7 @@
 package com.ssafy.board
 
 import com.ssafy.data.repository.board.BoardRepository
-import com.ssafy.model.board.MemberListResponseDTO
+import com.ssafy.model.board.MemberResponseDTO
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -9,7 +9,7 @@ class GetBoardMembersUseCase @Inject constructor(
     private val boardRepository: BoardRepository
 ) {
 
-    suspend operator fun invoke(boardId: Long): Flow<MemberListResponseDTO> {
+    suspend operator fun invoke(boardId: Long): Flow<List<MemberResponseDTO>> {
         return boardRepository.getBoardMembers(boardId)
     }
 
