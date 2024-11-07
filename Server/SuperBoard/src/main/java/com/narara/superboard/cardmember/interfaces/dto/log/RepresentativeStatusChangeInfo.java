96433e671f5f4320c6af7cbf5299f940301be126
조시현 <si4018@naver.com>
2024-11-07 14:32:20 +0900

    [BE] ♻️ refactor: 필드명 변경

--- Server/SuperBoard/src/main/java/com/narara/superboard/cardmember/interfaces/dto/log/RepresentativeStatusChangeInfo.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/cardmember/interfaces/dto/log/RepresentativeStatusChangeInfo.java
@@ -5,7 +5,7 @@ package com.narara.superboard.cardmember.interfaces.dto.log;
 // CardMember 대표 상태 변경 관련 정보
 public record RepresentativeStatusChangeInfo(
         Long memberId,
-        String memberName,
+        String memberNickname,
         Long cardId,
         String cardName,
         boolean isRepresentative
