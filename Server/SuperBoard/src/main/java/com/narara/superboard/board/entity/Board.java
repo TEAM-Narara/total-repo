40695d6a87951efb86e18c7cfa4a794f24a28dc4
lime <joohr1234@gmail.com>
2024-11-07 17:28:33 +0900

    [BE] 🐛 fix: board 수정 시 null값이 들어오면 수정안되도록 변경 누락 추가 - name null처리 누락 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/board/entity/Board.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/board/entity/Board.java
@@ -102,7 +102,7 @@ public class Board extends BaseTimeEntity implements Identifiable {
             this.cover.put("type", boardUpdateRequestDto.cover().type());
             this.cover.put("value", boardUpdateRequestDto.cover().value());
         }
-        if (!(boardUpdateRequestDto.name().isEmpty() || boardUpdateRequestDto.name().isBlank())) {
+        if (!(boardUpdateRequestDto.name() == null || boardUpdateRequestDto.name().isEmpty() || boardUpdateRequestDto.name().isBlank())) {
             this.name = boardUpdateRequestDto.name();
         }
         if (!(boardUpdateRequestDto.visibility() == null || boardUpdateRequestDto.visibility().isEmpty() || boardUpdateRequestDto.visibility().isBlank())) {
