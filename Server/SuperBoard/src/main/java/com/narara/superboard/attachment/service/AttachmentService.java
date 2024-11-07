7e87ede14b6816639d344e3e09c68a7e8251dfde
sihyunjojo <si4018@naver.com>
2024-11-08 07:53:17 +0900

    [BE] 🎨 style: 작업 한 TODO 삭제

--- Server/SuperBoard/src/main/java/com/narara/superboard/attachment/service/AttachmentService.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/attachment/service/AttachmentService.java
@@ -4,10 +4,7 @@ import com.narara.superboard.attachment.entity.Attachment;
 import com.narara.superboard.member.entity.Member;
 
 public interface AttachmentService {
-    // TODO : 첨부파일 등록
     Attachment addAttachment(Member member, Long cardId, String url);
-    // TODO : 첨부파일 삭제
     void deleteAttachment(Member member, Long attachmentId);
-    // TODO : 첨부파일 커버 여부 수정
     void updateAttachmentIsCover(Long attachmentId);
 }
