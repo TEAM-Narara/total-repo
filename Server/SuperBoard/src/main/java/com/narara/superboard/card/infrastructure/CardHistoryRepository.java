43bed7a7eed2001c22ef07909acfe1ff3ea415c0
조시현 <si4018@naver.com>
2024-11-07 10:47:32 +0900

    [BE] ✨ feat: 보드id에 대한 히스토리 조회 로직 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardHistoryRepository.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/infrastructure/CardHistoryRepository.java
@@ -3,5 +3,8 @@ package com.narara.superboard.card.infrastructure;
 import com.narara.superboard.card.document.CardHistory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CardHistoryRepository extends MongoRepository<CardHistory, String> {
+    List<CardHistory> findByWhere_BoardIdOrderByWhenDesc(Long boardId);
 }
