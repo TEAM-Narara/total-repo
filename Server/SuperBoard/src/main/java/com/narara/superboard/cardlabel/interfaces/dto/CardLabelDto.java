0710799ceb4600022ed414a36673d7763888ed00
sihyunjojo <si4018@naver.com>
2024-11-08 07:49:42 +0900

    [BE] ✨ feat: DTO 생성 메서드 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/cardlabel/interfaces/dto/CardLabelDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/cardlabel/interfaces/dto/CardLabelDto.java
@@ -1,5 +1,19 @@
 package com.narara.superboard.cardlabel.interfaces.dto;
 
-public record CardLabelDto(Long labelId,String name,Long color,Boolean isCardLabel) {
+import com.narara.superboard.cardlabel.entity.CardLabel;
+import com.narara.superboard.label.entity.Label;
+import lombok.Builder;
+
+@Builder
+public record CardLabelDto(Long labelId, String name, Long color, Boolean IsActivated) {
+    public static CardLabelDto of(CardLabel cardLabel){
+        Label label = cardLabel.getLabel();
+        return CardLabelDto.builder()
+                .labelId(label.getId())
+                .name(label.getName())
+                .color(label.getColor())
+                .IsActivated(cardLabel.getIsActivated())
+                .build();
+    }
 }
 
