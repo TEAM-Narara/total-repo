02e381a6ef50f363ba510e29acb647b119268333
sihyunjojo <si4018@naver.com>
2024-11-08 07:47:01 +0900

    [BE] ✨ feat: 멤버의 배경 API 생성

---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/memberbackground/interfaces/MemberBackgroundAPI.java
@@ -0,0 +1,30 @@
+package com.narara.superboard.memberbackground.interfaces;
+
+import com.narara.superboard.common.interfaces.response.DefaultResponse;
+import com.narara.superboard.memberbackground.entity.MemberBackground;
+import com.narara.superboard.memberbackground.interfaces.dto.MemberBackgroundResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/member-background")
+public interface MemberBackgroundAPI {
+
+    @PostMapping
+    @Operation(summary = "회원 배경 추가", description = "회원의 배경 이미지를 추가합니다.")
+    ResponseEntity<DefaultResponse<MemberBackgroundResponseDto>> addMemberBackground(
+            @RequestParam Long memberId,
+            @RequestParam String imgUrl);
+
+    @GetMapping("/{memberId}")
+    @Operation(summary = "회원 배경 리스트 조회", description = "회원의 모든 배경 이미지를 조회합니다.")
+    ResponseEntity<DefaultResponse<List<MemberBackgroundResponseDto>>> getAllMemberBackground(@PathVariable Long memberId);
+
+    @DeleteMapping("/{memberId}/{backgroundId}")
+    @Operation(summary = "회원 배경 삭제", description = "특정 배경 이미지를 삭제합니다.")
+    ResponseEntity<DefaultResponse<Void>> deleteMemberBackground(
+            @PathVariable Long memberId,
+            @PathVariable Long backgroundId);
+}
---
+++ Server/SuperBoard/src/main/java/com/narara/superboard/memberbackground/interfaces/MemberBackgroundController.java
@@ -0,0 +1,66 @@
+package com.narara.superboard.memberbackground.interfaces;
+
+import com.narara.superboard.common.interfaces.response.DefaultResponse;
+import com.narara.superboard.common.interfaces.response.ResponseMessage;
+import com.narara.superboard.common.interfaces.response.StatusCode;
+import com.narara.superboard.memberbackground.entity.MemberBackground;
+import com.narara.superboard.memberbackground.interfaces.dto.MemberBackgroundResponseDto;
+import com.narara.superboard.memberbackground.service.MemberBackgroundService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "회원 배경")
+@Controller
+@RequiredArgsConstructor
+public class MemberBackgroundController implements MemberBackgroundAPI {
+
+    private final MemberBackgroundService memberBackgroundService;
+
+    @Override
+    public ResponseEntity<DefaultResponse<MemberBackgroundResponseDto>> addMemberBackground(
+            @RequestParam Long memberId,
+            @RequestParam String imgUrl) {
+
+        MemberBackground memberBackground = memberBackgroundService.addMemberBackground(memberId, imgUrl);
+        return new ResponseEntity<>(
+                (DefaultResponse.res(StatusCode.CREATED, ResponseMessage.MEMBER_BACKGROUND_ADD_SUCCESS,
+                        MemberBackgroundResponseDto.of(memberBackground))),
+                HttpStatus.CREATED
+        );
+    }
+
+    @Override
+    public ResponseEntity<DefaultResponse<List<MemberBackgroundResponseDto>>> getAllMemberBackground(
+            @PathVariable Long memberId) {
+
+        List<MemberBackground> backgrounds = memberBackgroundService.getAllMemberBackground(memberId);
+        List<MemberBackgroundResponseDto> memberBackgroundResponseDtoList= new ArrayList<>();
+        for (MemberBackground background : backgrounds) {
+            memberBackgroundResponseDtoList.add(MemberBackgroundResponseDto.of(background));
+        }
+        return new ResponseEntity<>(
+                DefaultResponse.res(StatusCode.OK, ResponseMessage.MEMBER_BACKGROUND_LIST_FETCH_SUCCESS,
+                        memberBackgroundResponseDtoList),
+                HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<DefaultResponse<Void>> deleteMemberBackground(
+            @PathVariable Long memberId,
+            @PathVariable Long backgroundId) {
+
+        memberBackgroundService.deleteMemberBackground(memberId, backgroundId);
+        return new ResponseEntity<>(
+                DefaultResponse.res(StatusCode.OK, ResponseMessage.MEMBER_BACKGROUND_DELETE_SUCCESS),
+                HttpStatus.OK
+        );
+    }
+}
