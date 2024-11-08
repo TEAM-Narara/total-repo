9a605208559ca417b6bfb81f748529ede1fd8e5a
Juhyorim <joohr1234@gmail.com>
2024-11-09 00:34:09 +0900

    [BE] 🐛 fix: reply response 로 관련있는 값(필요할 것 같은 값) 모두 주도록 변경 - memberId - isDeleted

--- Server/SuperBoard/src/main/java/com/narara/superboard/reply/interfaces/dto/ReplySimpleResponseDto.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/reply/interfaces/dto/ReplySimpleResponseDto.java
@@ -2,14 +2,15 @@ package com.narara.superboard.reply.interfaces.dto;
 
 import com.narara.superboard.reply.entity.Reply;
 import com.narara.superboard.workspace.interfaces.dto.websocket.WebSocketData;
-import com.narara.superboard.workspace.interfaces.dto.websocket.WebSocketResponse;
 import lombok.Builder;
 
 @Builder
 public record ReplySimpleResponseDto(
     Long cardId,
     Long replyId,
+    Long memberId,
     String content,
+    boolean isDeleted,
     Long lastUpdatedAt
 ) implements WebSocketData {
 
@@ -17,7 +18,9 @@ public record ReplySimpleResponseDto(
         return ReplySimpleResponseDto.builder()
                 .replyId(reply.getId())
                 .cardId(reply.getCard().getId())
+                .memberId(reply.getMember().getId())
                 .content(reply.getContent())
+                .isDeleted(reply.getIsDeleted())
                 .lastUpdatedAt(reply.getUpdatedAt())
                 .build();
     }
