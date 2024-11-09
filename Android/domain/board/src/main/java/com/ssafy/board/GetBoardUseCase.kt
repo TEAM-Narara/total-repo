9943e4440724ed83c0d18cde307f36630988d119
박준식 <akfldkskgorn@gmail.com>
2024-11-09 21:12:51 +0900

    [FE] ✨ feat: board stomp connect 연결

--- Android/domain/board/src/main/java/com/ssafy/board/GetBoardUseCase.kt
+++ Android/domain/board/src/main/java/com/ssafy/board/GetBoardUseCase.kt
@@ -1,16 +1,19 @@
 package com.ssafy.board
 
 import com.ssafy.data.repository.board.BoardRepository
+import com.ssafy.data.socket.board.BoardStomp
 import com.ssafy.model.board.BoardDTO
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetBoardUseCase @Inject constructor(
+    private val boardStomp: BoardStomp,
     private val boardRepository: BoardRepository
 ) {
 
     suspend operator fun invoke(id: Long): Flow<BoardDTO>? {
-        return  boardRepository.getBoard(id)
+        boardStomp.connect(id)
+        return boardRepository.getBoard(id)
     }
 
 }
