bb634d826aa1788fc5539302824d1ae448cca790
조시현 <si4018@naver.com>
2024-11-11 18:01:29 +0900

    [BE] ✨ feat: 재배치 로직 시, 순서 적용되게 추가

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardReorderServiceImpl.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardReorderServiceImpl.java
@@ -21,17 +21,25 @@ public class CardReorderServiceImpl implements CardReorderService {
 
     @Override
     @Transactional
-    public java.util.List<CardMoveResponseDto> reorderAllCardOrders(List list) {
+    public java.util.List<CardMoveResponseDto> reorderAllCardOrders(List list, Card targetCard, int targetIndex) {
         // 해당 보드의 모든 카드를 myOrder 기준으로 오름차순 정렬하여 조회
         java.util.List<Card> cards = cardRepository.findAllByListOrderByMyOrderAsc(list);
+
+        // targetIndex -1일 경우 가장 아래로 삽입
+        if (targetIndex == -1) {
+            cards.remove(targetCard);
+            cards.add(targetCard);  // 가장 마지막에 삽입
+        } else {
+            cards.remove(targetCard);
+            cards.add(targetIndex, targetCard);  // 특정 위치에 삽입
+        }
 
         // 초기 값 설정 및 결과 리스트 생성
         long newOrder = MoveConst.DEFAULT_TOP_ORDER;
         java.util.List<CardMoveResponseDto> orderInfoList = new ArrayList<>();
-
         for (Card card : cards) {
             card.setMyOrder(newOrder);
-            orderInfoList.add(new CardMoveResponseDto(card.getId(), newOrder));
+            orderInfoList.add(new CardMoveResponseDto(card.getId(), list.getId(), newOrder));
             newOrder += REORDER_GAP;
         }
 
