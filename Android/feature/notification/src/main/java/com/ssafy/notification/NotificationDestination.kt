f12308e618965a8faac55a49c3325645624e0428
최승준 <csjune99@naver.com>
2024-11-17 01:35:28 +0900

    [FE] ✨ feat: 에러 핸들링

--- Android/feature/notification/src/main/java/com/ssafy/notification/NotificationDestination.kt
+++ Android/feature/notification/src/main/java/com/ssafy/notification/NotificationDestination.kt
@@ -1,5 +1,7 @@
 package com.ssafy.notification
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
@@ -11,7 +13,14 @@ fun NavGraphBuilder.notificationScreen(
     popBack: () -> Unit,
 ) {
     composable<Notification> {
+        val viwModel: NotificationViewModel = hiltViewModel<NotificationViewModel>().apply {
+            LaunchedEffect(Unit) {
+                getNotificationList()
+            }
+        }
+
         NotificationScreen(
+            viewModel = viwModel,
             popBackStack = popBack
         )
     }
