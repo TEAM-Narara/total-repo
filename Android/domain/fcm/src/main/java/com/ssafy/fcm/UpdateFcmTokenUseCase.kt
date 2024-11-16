3cb9d02fe87777e995919670232f94f6bcd71341
최승준 <csjune99@naver.com>
2024-11-16 18:31:33 +0900

    [FE] ✨ feat: fcm 수정

--- Android/domain/fcm/src/main/java/com/ssafy/fcm/UpdateFcmTokenUseCase.kt
+++ Android/domain/fcm/src/main/java/com/ssafy/fcm/UpdateFcmTokenUseCase.kt
@@ -4,14 +4,16 @@ import com.ssafy.data.repository.fcm.FcmRepository
 import com.ssafy.datastore.DataStoreRepository
 import com.ssafy.model.fcm.FcmDTO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class UpdateFcmTokenUseCase @Inject constructor(
     private val dataStoreRepository: DataStoreRepository,
     private val fcmRepository: FcmRepository
 ) {
-    suspend operator fun invoke(fcmToken: String): Flow<FcmDTO> {
+    suspend operator fun invoke(fcmToken: String): Flow<FcmDTO> = flow {
         val memberId = dataStoreRepository.getUser().memberId
-        return fcmRepository.updateFcmToken(memberId, fcmToken)
+        if (memberId == 0L) return@flow
+        fcmRepository.updateFcmToken(memberId, fcmToken).collect { emit(it) }
     }
 }
