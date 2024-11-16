307e51558a328395cb79eba95a879cda87c9448e
Juhyorim <joohr1234@gmail.com>
2024-11-16 16:13:23 +0900

    [BE] ✨ feat: Card 이동 알림 형식 구현 - 완전히 다른 리스트로의 이동일 때만 알림이 울림

--- Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardMoveService.java
+++ Server/SuperBoard/src/main/java/com/narara/superboard/card/service/CardMoveService.java
@@ -1,5 +1,6 @@
 package com.narara.superboard.card.service;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.narara.superboard.card.interfaces.dto.CardMoveCollectionRequest;
 import com.narara.superboard.card.interfaces.dto.CardMoveResult;
 import com.narara.superboard.member.entity.Member;
@@ -11,5 +12,6 @@ public interface CardMoveService {
 
     CardMoveResult moveCardBetween(Member member, Long cardId, Long previousCardId, Long nextCardId);
 
-    CardMoveResult moveCardVersion2(Member member, Long listId, CardMoveCollectionRequest cardMoveCollectionRequest);
+    CardMoveResult moveCardVersion2(Member member, Long listId, CardMoveCollectionRequest cardMoveCollectionRequest)
+            throws FirebaseMessagingException;
 }
