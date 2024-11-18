8d87dce35215b581ee4db5b7b4d34a604c7ac2fe
sihyunjojo <si4018@naver.com>
2024-11-19 01:32:40 +0900

    [BE] ♻️ refactor: 테스트 변경

--- Server/eureka/src/test/java/com/narara/eureka/EurekaApplicationTests.java
+++ Server/eureka/src/test/java/com/narara/eureka/EurekaApplicationTests.java
@@ -1,15 +1,24 @@
 package com.narara.eureka;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class EurekaApplicationTests {
+
+    @Autowired
+    private ApplicationContext context; // ApplicationContext 주입
 
     @Test
     void contextLoads() {
+        Environment environment = context.getEnvironment(); // Environment 가져오기
+        System.out.println("Environment variables: " + System.getenv());
+        System.out.println("Active profiles: " + Arrays.toString(environment.getActiveProfiles()));
     }
+
 
 }
