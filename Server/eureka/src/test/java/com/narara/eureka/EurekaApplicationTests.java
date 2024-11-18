7f6475e615760aba2d531faada3a536077ac8769
sihyunjojo <si4018@naver.com>
2024-11-19 01:49:40 +0900

    [BE] ♻️ refactor: 테스트 변경

--- Server/eureka/src/test/java/com/narara/eureka/EurekaApplicationTests.java
+++ Server/eureka/src/test/java/com/narara/eureka/EurekaApplicationTests.java
@@ -1,22 +1,8 @@
 package com.narara.eureka;
 
-import com.netflix.discovery.DiscoveryClient;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class EurekaApplicationTests {
-
-    // Eureka 관련 빈을 목(Mock)으로 대체
-    @MockBean
-    private DiscoveryClient discoveryClient;
-
-
-    @Test
-    void contextLoads() {
-    }
 
 }
